c94d82a9319b26f8d517b4c7df940734f62d889f
Volodymyr <volodymyrquo@gmail.com>
2020-12-14 17:23:29 +0200

    created Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div className='container'>
       <div className='app-wrapper'>
-        <h1>App</h1>
+        <Header />
       </div>
     </div>
   );
