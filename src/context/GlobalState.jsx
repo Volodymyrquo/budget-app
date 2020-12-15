dcdadc1aa1f908486c74b406f882789099763175
Volodymyr <volodymyrquo@gmail.com>
2020-12-15 07:07:39 +0200

    added state to browser local storage

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,23 +1,28 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-  incomeTransactions: [
-    { id: 1, incomeText: 'Car sold', incomeAmount: 15000 },
-    { id: 2, incomeText: 'Salary', incomeAmount: 5000 },
-    { id: 3, incomeText: 'Bonus', incomeAmount: 13000 },
-  ],
-  expenseTransactions: [
-    { id: 4, expenseText: 'Rent', expenseAmount: 1000 },
-    { id: 5, expenseText: 'Bank', expenseAmount: 2000 },
-    { id: 6, expenseText: 'Clothes', expenseAmount: 500 },
-  ],
+  incomeTransactions:
+    JSON.parse(localStorage.getItem('incomeTransactions')) || [],
+  expenseTransactions:
+    JSON.parse(localStorage.getItem('expenseTransactions')) || [],
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+  useEffect(() => {
+    localStorage.setItem(
+      'incomeTransactions',
+      JSON.stringify(state.incomeTransactions)
+    );
+    localStorage.setItem(
+      'expenseTransactions',
+      JSON.stringify(state.expenseTransactions)
+    );
+  });
+
   const addIncome = (incomeTransaction) => {
     dispatch({
       type: 'ADD_INCOME',
